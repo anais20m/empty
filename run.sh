ac25c37fbd6a09ef8775a957c5acecb0cf1ec098
anais20m <86193547+anais20m@users.noreply.github.com>
2022-07-01 14:24:08 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ cd android/lineage
 repo sync --force-sync -j8
 device trees
 git clone https://github.com/ArrowOS-Devices/android_kernel_gm_msm8937 kernel/GM/GM8_sprout
-git clone https://github.com/Headache01/android_device_GM_GM8_sprout device/GM/GM8_sprout
-git clone https://github.com/Headache01/android_vendor_GM_GM8_sprout vendor/GM/GM8_sprout
+git clone https://github.com/yigityanik/android_device_GM_GM8_sprout device/GM/GM8_sprout
+git clone https://github.com/yigityanik/android_vendor_GM_GM8_sprout vendor/GM/GM8_sprout
 
 #build
 source build/envsetup.sh
